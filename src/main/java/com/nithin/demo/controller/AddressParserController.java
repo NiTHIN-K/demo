98870b59e91b32726a390400ae2a342971ec558a
NiTHiNK <super0blue0@gmail.com>
2023-09-29 13:57:54 -0700

    validate address with api

--- src/main/java/com/nithin/demo/controller/AddressParserController.java
+++ src/main/java/com/nithin/demo/controller/AddressParserController.java
@@ -3,10 +3,7 @@ package com.nithin.demo.controller;
 import com.nithin.demo.services.AddressParserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -19,7 +16,7 @@ public class AddressParserController {
         this.addressParserService = addressParserService;
     }
 
-    @PostMapping("/parse-address")
+    @GetMapping("/parse-address")
     public ResponseEntity<String> parseAddress(@RequestBody String address) {
         String upp = addressParserService.parseToUPP(address);
         return ResponseEntity.ok(upp);
--- src/main/java/com/nithin/demo/services/AddressParserService.java
+++ src/main/java/com/nithin/demo/services/AddressParserService.java
@@ -1,12 +1,48 @@
 package com.nithin.demo.services;
 
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class AddressParserService {
     public String parseToUPP(String address) {
-        return "UPP representation of the address";
-        // Implement UPP parsing logic here
-        // Return the UPP representation of the address
+        RestTemplate restTemplate = new RestTemplate();
+        String baseUrl = "https://www.yaddress.net";
+        String path = "/api/address/";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl).path(path);
+        builder.queryParam("UserKey", "");
+        builder.queryParam("AddressLine2", address);
+        String url = builder.build().toUriString();
+
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("AddressLine2", address);
+        String response = restTemplate.getForObject(url, String.class, uriVariables);
+//        System.out.println(response);
+        JsonParser springParser = JsonParserFactory.getJsonParser();
+        Map< String, Object > map = springParser.parseMap(response);
+        String mapArray[] = new String[map.size()];
+        System.out.println("Items found: " + mapArray.length);
+        int i = 0;
+        for (Map.Entry < String, Object > entry: map.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+            i++;
+        }
+        if((int)map.get("ErrorCode") != 0){
+            return "unrecognized address format";
+        }
+
+
+
+        return "UPP representation of the address " + response;
     }
 }
